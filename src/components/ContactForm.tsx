bd026556cedf36abe115e1edd9c4b7266be1e146
Heroic Matt <florinvenis@gmail.com>
2024-08-17 22:05:47 +0300

    Added contact spinner

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -1,14 +1,17 @@
 'use client'
 
 import { Mail, Pencil, Send, User } from "lucide-react";
-import { createRef, FormEvent } from "react";
+import { createRef, FormEvent, useRef, useState } from "react";
 import { useToast } from "./ui/use-toast";
 
 export default function ContactForm(){
     const { toast } = useToast()
-    const ref = createRef<HTMLFormElement>();
+    const formRef = useRef<HTMLFormElement>(null);
+
+    const [loading, setLoading] = useState(false)
 
     async function handleSubmit(event: FormEvent<HTMLFormElement>) {
+        setLoading(true)
         event.preventDefault();
 
         const formData = new FormData(event.currentTarget);
@@ -18,7 +21,6 @@ export default function ContactForm(){
     
         const object = Object.fromEntries(formData);
         const json = JSON.stringify(object);
-    
         const response = await fetch("https://api.web3forms.com/submit", {
             method: "POST",
             headers: {
@@ -28,8 +30,10 @@ export default function ContactForm(){
             body: json
         });
         const result = await response.json();
+        setLoading(false)
+        
         if (result.success) {
-            ref.current!.reset();
+            formRef.current!.reset()
             toast({
                 title:"Your message has been sent.",
                 description: "You will receive your answer shortly"
@@ -46,7 +50,7 @@ export default function ContactForm(){
   
     return(
       <>
-        <form className='flex flex-col w-1/2 mt-8 border-4 p-8 bg-white max-xl:w-full' onSubmit={handleSubmit} method="POST" ref={ref}>
+        <form className='flex flex-col w-1/2 mt-8 border-4 p-8 bg-white max-xl:w-full' onSubmit={handleSubmit} method="POST" ref={formRef}>
           <label className='flex flex-row items-center gap-1'><User className='size-6'/> Name</label>
           <input required type="text" name="name" placeholder="Enter your name" className='border-black border h-8 mb-4 mt-1 px-2'/>
           <label className='flex flex-row items-center gap-1'><Mail className='size-6'/> Email</label>
@@ -58,7 +62,22 @@ export default function ContactForm(){
             <p>I confirm sending this message.</p>
           </div>
           <div className='mt-4'>
-            <button type='submit' className='primary-light-btn hover:bg-dark-blue hover:text-white'><Send/>Send</button>
+            <button type='submit' className='primary-light-btn hover:bg-dark-blue hover:text-white' disabled={loading}>
+                {loading ? (
+                    <>
+                        <div className="inline-block h-6 w-6 animate-spin rounded-full border-4 border-solid border-current border-e-transparent align-[-0.125em] text-surface motion-reduce:animate-[spin_1.5s_linear_infinite] dark:text-white" role="status">
+                            <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
+                                Loading...
+                            </span>
+                        </div>  
+                        Loading 
+                    </>
+                ) : (
+                    <>
+                        <Send/>Send
+                    </>
+                )}
+            </button>
           </div>
   
           <label className='flex flex-row items-center gap-1 mt-8 cursor-text max-md:flex-col'><Mail className='size-6'/> <span className='font-semibold'>Work Email:</span> florinvenis.dev.business@gmail.com</label>
