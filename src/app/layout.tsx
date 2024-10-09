f4af2c4b4d7714d0880f11f849aa17904029c8cc
Heroic Matt <florinvenis@gmail.com>
2024-10-09 21:56:09 +0300

    Minor change to page title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,7 @@ import { Toaster } from "@/components/ui/toaster";
 const montserrat = Montserrat({ subsets:["latin", "latin-ext"]})
 
 export const metadata: Metadata = {
-  title: "Personal Portfolio",
+  title: "Florin Venis | Personal Portfolio",
   description: "My name is Florin Venis, I'm a 19 year old, IT Instructor, Freelance Developer from Romania. With over 6 years of programming experience and numerous finished projects, I enjoy building software using the latest technologies.",
 };
 
